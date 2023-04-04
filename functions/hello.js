766a4a93a5185c4f4fd2d0807c76534610ae20f1
Alexander Akselrod <aleksandr.akselrod@sbermegamarket.ru>
2023-04-04 16:53:13 +0300

    added authentication

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,6 @@
 export function onRequest (contect) {
     const { request } = context;
     console.log(`Request came from ${request.url}`)
-    
+    asdsdasdasdasdasddasdadasdasdasdasdasdasdasd
     return new Response ("Hello, world!")
 }
