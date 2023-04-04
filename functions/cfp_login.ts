bc3c1556078957acf6515ffcd07e234eac232cc1
Alexander Akselrod <aleksandr.akselrod@sbermegamarket.ru>
2023-04-04 22:00:19 +0300

    added logs

--- functions/cfp_login.ts
+++ functions/cfp_login.ts
@@ -2,37 +2,37 @@ import { CFP_COOKIE_MAX_AGE } from './constants';
 import { sha256, getCookieKeyValue } from './utils';
 
 export async function onRequestPost(context: {
-  request: Request;
-  env: { CFP_PASSWORD?: string };
+    request: Request;
+    env: { CFP_PASSWORD?: string };
 }): Promise<Response> {
-  const { request, env } = context;
-  const body = await request.formData();
+    const { request, env } = context;
+    const body = await request.formData();
     const { password, redirect } = Object.fromEntries(body);
     console.log(`got ${password} while settings is ${env.CFP_PASSWORD} and redirect is ${redirect}`)
-  const hashedPassword = await sha256(password.toString());
-  const hashedCfpPassword = await sha256(env.CFP_PASSWORD);
-  const redirectPath = redirect.toString() || '/';
+    const hashedPassword = await sha256(password.toString());
+    const hashedCfpPassword = await sha256(env.CFP_PASSWORD);
+    //  const redirectPath = redirect.toString() || '/';
+    const redirectPath = "/"
+    if (hashedPassword === hashedCfpPassword) {
+	// Valid password. Redirect to home page and set cookie with auth hash.
+	const cookieKeyValue = await getCookieKeyValue(env.CFP_PASSWORD);
 
-  if (hashedPassword === hashedCfpPassword) {
-    // Valid password. Redirect to home page and set cookie with auth hash.
-    const cookieKeyValue = await getCookieKeyValue(env.CFP_PASSWORD);
-
-    return new Response('', {
-      status: 302,
-      headers: {
-        'Set-Cookie': `${cookieKeyValue}; Max-Age=${CFP_COOKIE_MAX_AGE}; Path=/; HttpOnly; Secure`,
-        'Cache-Control': 'no-cache',
-        Location: redirectPath
-      }
-    });
-  } else {
-    // Invalid password. Redirect to login page with error.
-    return new Response('', {
-      status: 302,
-      headers: {
-        'Cache-Control': 'no-cache',
-        Location: `${redirectPath}?error=1`
-      }
-    });
-  }
+	return new Response('', {
+	    status: 302,
+	    headers: {
+		'Set-Cookie': `${cookieKeyValue}; Max-Age=${CFP_COOKIE_MAX_AGE}; Path=/; HttpOnly; Secure`,
+		'Cache-Control': 'no-cache',
+		Location: redirectPath
+	    }
+	});
+    } else {
+	// Invalid password. Redirect to login page with error.
+	return new Response('', {
+	    status: 302,
+	    headers: {
+		'Cache-Control': 'no-cache',
+		Location: `${redirectPath}?error=1`
+	    }
+	});
+    }
 }
